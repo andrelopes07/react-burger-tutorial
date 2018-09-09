f0e0e8fd7ca84a6724d62589ba044ec560efe3df
André Lopes <dx_andrelopes@hotmail.com>
2018-09-09 23:11:44 +0100

    Routing added

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
-import CheckouSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
     state = {
-        ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 1,
-            meat: 1
+        ingredients: {}
+    }
+
+    componentDidMount () {
+        const query = new URLSearchParams(this.props.location.search);
+
+        const ingredients = {};
+
+        for (let param of query.entries()) {
+            ingredients[param[0]] = +param[1];
         }
+
+        this.setState({ ingredients: ingredients });
+    }
+
+    checkoutCancelled = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinued = () => {
+        this.props.history.replace('/checkout/contact-data');
     }
 
     render () {
         return (
             <div>
-                <CheckouSummary ingredients={this.state.ingredients} />
+                <CheckoutSummary
+                    ingredients={this.state.ingredients}
+                    checkoutCancelled={this.checkoutCancelled}
+                    checkoutContinued={this.checkoutContinued}
+                />
+                <Route path={`${this.props.match.url}/contact-data`} component={ContactData} />
             </div>
         );
     }
